9dc5231bad2d74d202085c8462657ec673700df9
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-03-17 15:52:29 +0100

    Adding of the authentification pages (signup, signin, account and edit PSW). Putting header,main and footer in display grid with header and footer sticky.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/authentification/signin.html"),
+    new Route("/signup", "Inscription", "/pages/authentification/signup.html"),
+    new Route("/account", "Mon compte", "/pages/authentification/account.html"),
+    new Route("/editPSW", "Editer mot de passe", "/pages/authentification/editPSW.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
