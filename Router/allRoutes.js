7e18f68ed81fa0a207835eed91a1a96c5fc6996a
Emilien-GIBEAUD <emiliengibeaud@gmail.com>
2025-03-18 10:56:40 +0100

    Update the links and the router.js (window.event depreciated)

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,12 +4,13 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/menu", "La carte", "/pages/menu.html"),
     new Route("/allReservations", "Mes réservations", "/pages/reservations/allReservations.html"),
     new Route("/reserve", "Réserver", "/pages/reservations/reserve.html"),
     new Route("/account", "Mon compte", "/pages/authentification/account.html"),
     new Route("/editPSW", "Editer mot de passe", "/pages/authentification/editPSW.html"),
     new Route("/signin", "Connexion", "/pages/authentification/signin.html"),
-    new Route("/signup", "Inscription", "/pages/authentification/signup.html"),
+    new Route("/signup", "Inscription", "/pages/authentification/signup.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
